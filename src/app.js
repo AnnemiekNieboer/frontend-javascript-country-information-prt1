effd85a6a1281d14c837281786aa3acb52a3983b
AnnemiekNieboer <annemieknieboer@gmail.com>
2022-06-01 16:32:08 +0200

    Basic assignment done

--- src/app.js
+++ src/app.js
@@ -1,90 +1,80 @@
 import axios from "axios";
 console.log('Hallo daar!');
 
-// function changeColorOfCountryName(region) {
-//     const countryName = document.getElementsByClassName("country-class");
-//     if (region === "Asia") {
-//         countryName.classList.add("asia");
-//     } if (region === "Europe") {
-//         countryName.classList.add("europe");
-//     } if (region === "Africa") {
-//         countryName.classList.add("africa");
-//     } if (region === "Americas") {
-//         countryName.classList.add("america");
-//     } if (region === "Oceania") {
-//         countryName.classList.add("oceania") ;
-//     } else {
-//         countryName.classList.add("otherCountries");
-//     }
-// }
-
-// function changeColorOfCountryName(region) {
-//     const countryName = document.getElementsByTagName("h4");
-//     if (region === "Asia") {
-//         countryName.className += "asia";
-//     } if (region === "Europe") {
-//         countryName.className += "europe";
-//     } if (region === "Africa") {
-//         countryName.className += "africa";
-//     } if (region === "Americas") {
-//         countryName.className += "america";
-//     } if (region === "Oceania") {
-//         countryName.className += "oceania";
-//     } else {
-//         countryName.classList += "otherCountries";
-//     }
-// }
-
-//Versie zonder map
-async function fetchAllCountrydata() {
-    try {
-        const response = await axios.get("https://restcountries.com/v2/all");
-        // console.log(response.data[0]);
-        const responseAddedInHTML = document.getElementById("list-of-countries");
-
-        responseAddedInHTML.innerHTML = `
-        <li>
-            <h4 class="country-class">${response.data[0].name}</h4>
-            <p>
-            Has a population of ${response.data[0].population} people
-            </p>
-            <img src="${response.data[0].flags.png}" alt="flag of ${response.data[0].name}">
-        </li>
-        `
-        // changeColorOfCountryName(response.data[0].region);
-
-        // console.log(`Regio selectie = ${response.data[0].region}`);
-        // console.log(document.getElementsByTagName("h4"));
-    } catch (error) {
-        console.log(error)
+function changeColorOfCountryName(region) {
+    switch(region) {
+        case "Asia":
+            return "asia";
+        case "Europe":
+            return "europe";
+        case "Africa":
+            return "africa";
+        case "Americas":
+            return "america"
+        case "Oceania":
+            return "oceania"
+        default:
+            return "otherCountries"
     }
 }
 
-//versie met map
+
+//Versie zonder map
 // async function fetchAllCountrydata() {
 //     try {
 //         const response = await axios.get("https://restcountries.com/v2/all");
-//         console.log(response);
+//         // console.log(response.data[0]);
+//         const responseAddedInHTML = document.getElementById("list-of-countries");
 //
-//         const listOfCountries = response.data;
-//         console.log(listOfCountries);
-//         let responseAddedInHTML = document.getElementById("list-of-countries");
+//         responseAddedInHTML.innerHTML = `
+//         <li>
+//             <h4 class="country-class">${response.data[0].name}</h4>
+//             <p>
+//             Has a population of ${response.data[0].population} people
+//             </p>
+//             <img src="${response.data[0].flags.png}" alt="flag of ${response.data[0].name}">
+//         </li>
+//         `
+//         // changeColorOfCountryName(response.data[0].region);
 //
-//         listOfCountries.map((country)) => {
-//             responseAddedInHTML.innerHTML += `
-//             <li>
-//                 <h4 class="country-class">${country.name}</h4>
-//                 <p>
-//                 Has a population of ${country.population} people
-//                 </p>
-//                 <img src="${country.flags.png}" alt="flag of ${country.name}">
-//             </li>
-//             `
-//         }
-//
+//         // console.log(`Regio selectie = ${response.data[0].region}`);
+//         // console.log(document.getElementsByTagName("h4"));
 //     } catch (error) {
 //         console.log(error)
 //     }
 // }
 
+// versie met map
+async function fetchAllCountrydata() {
+    try {
+        const response = await axios.get("https://restcountries.com/v2/all");
+        const countries = response.data;
+        countries.sort((a,b) => {
+            return a.population - b.population;
+        });
+        countryList(countries);
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 fetchAllCountrydata();
+
+function countryList(countries) {
+    let responseAddedInHTML = document.getElementById("list-of-countries");
+
+    responseAddedInHTML.innerHTML = countries.map((country) => {
+        return `
+            <li>
+                <h4 class="${changeColorOfCountryName(country.region)}">${country.name}</h4>
+                <p>
+                Has a population of ${country.population} people
+                </p>
+                <img src="${country.flags.png}" alt="flag of ${country.name}">
+            </li>
+            `;
+    } ).join("");
+}
+
+
